2f0822ea473d0d6ff2c7262da0f9919f168285c1
Sandeept1986-web <thontlasandeep28@gmail.com>
2025-12-24 13:07:49 +0000

    day-3 changes

--- Day-3/modules/ec2_instance/main.tf
+++ Day-3/modules/ec2_instance/main.tf
@@ -2,8 +2,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "latest_ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical's AWS account ID
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
 resource "aws_instance" "example" {
-    ami = var.ami_value
+    ami = data.aws_ami.latest_ubuntu.id
     instance_type = var.instance_type_value
     subnet_id = var.subnet_id_value
     associate_public_ip_address = true
